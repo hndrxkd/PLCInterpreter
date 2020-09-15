1b50398a1e04cef01d86e9a4dc962d1b03162a7a
Kevin Denis <44208186+hndrxkd@users.noreply.github.com>
2020-09-15 15:44:39 -0400

    Add files via upload

---
+++ src/main/java/plc.interpreter/Regex.java
@@ -0,0 +1,22 @@
+package plc.interpreter;
+
+import java.util.regex.Pattern;
+
+/**
+ * Contains {@link Pattern} constants, which are compiled regular expressions.
+ * See the assignment page for resources on regex's as needed.
+ */
+public class Regex {
+
+    public static final Pattern
+            EMAIL = Pattern.compile("[A-Za-z0-9._-]+@[A-Za-z0-9-]*\\.[a-z]{2,3}"),
+            FILE_NAMES = Pattern.compile("^((.)+(\\..)?\\.(class|java))$").m,
+            EVEN_STRINGS = Pattern.compile("(.){20}|(.){18}|(.){16}|(.){14}|(.){12}|(.){10}"),
+            INTEGER_LIST = Pattern.compile("\\[((,(\\s){0,1}|)[(0-9)]+|)+\\]"),
+            IDENTIFIER = Pattern.compile(""),
+            NUMBER = Pattern.compile("^(\\+|-)?(\\d+\\.)?\\d+$"),
+            STRING = Pattern.compile("^(\")([^'\"\\\\]*(\\\\[bnrt'\"\\\\])*)*(\")$");
+
+    //https://www.oreilly.com/library/view/java-cookbook-3rd/9781449338794/ch04.html
+
+}
---
+++ src/test/java/plc.interpreter/RegexTests.java
@@ -0,0 +1,115 @@
+package plc.interpreter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+/**
+ * Contains JUnit tests for {@link Regex}. Tests declarations for steps 1 & 2
+ * are provided, you must add your own for step 3.
+ *
+ * To run tests, either click the run icon on the left margin or execute the
+ * gradle test task, which can be done by clicking the Gradle tab in the right
+ * sidebar and navigating to Tasks > verification > test Regex(double click to run).
+ */
+public class RegexTests {
+
+    /**
+     * This is a parameterized test for the {@link Regex#EMAIL} regex. The
+     * {@link ParameterizedTest} annotation defines this method as a
+     * parameterized test, and {@link MethodSource} tells JUnit to look for the
+     * static method {@link #testEmailRegex()}.
+     *
+     * For personal preference, I include a test name as the first parameter
+     * which describes what that test should be testing - this is visible in
+     * IntelliJ when running the tests.
+     */
+    @ParameterizedTest
+    @MethodSource
+    public void testEmailRegex(String test, String input, boolean success) {
+        test(input, Regex.EMAIL, success);
+    }
+
+    /**
+     * This is the factory method providing test cases for the parameterized
+     * test above - note that it is static, takes no arguments, and has the same
+     * name as the test. The {@link Arguments} object contains the arguments for
+     * each test to be passed to the function above
+     */
+    public static Stream<Arguments> testEmailRegex() {
+        return Stream.of(
+                Arguments.of("Alphanumeric", "thelegend27@gmail.com", true),
+                Arguments.of("UF Domain", "otherdomain@ufl.edu", true),
+                Arguments.of("Missing Domain Dot", "missingdot@gmailcom", false),
+                Arguments.of("Symbols", "symbols#$%@gmail.com", false),
+                Arguments.of("test", "test@test.com", true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testFileNamesRegex(String test, String input, boolean success) {
+        //this one is different as we're also testing the file name capture
+        Matcher matcher = test(input, Regex.FILE_NAMES, success);
+        if (success) {
+            Assertions.assertEquals(input.substring(0, input.indexOf(".")), matcher.group("name"));
+        }
+    }
+
+    public static Stream<Arguments> testFileNamesRegex() {
+        return Stream.of(
+                Arguments.of("Java File", "Regex.tar.java", true),
+                Arguments.of("Java Class", "RegexTests.class", true),
+                Arguments.of("Directory", "directory", false),
+                Arguments.of("Python File", "scrippy.py", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testEvenStringsRegex(String test, String input, boolean success) {
+        test(input, Regex.EVEN_STRINGS, success);
+    }
+
+    public static Stream<Arguments> testEvenStringsRegex() {
+        return Stream.of(
+                Arguments.of("14 Characters", "thishas14chars", true),
+                Arguments.of("10 Characters", "i<3pancakes!", true),
+                Arguments.of("6 Characters", "6chars", false),
+                Arguments.of("15 Characters", "i<3pancakes!!", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testIntegerListRegex(String test, String input, boolean success) {
+        test(input, Regex.INTEGER_LIST, success);
+    }
+
+    public static Stream<Arguments> testIntegerListRegex() {
+        return Stream.of(
+                Arguments.of("Empty List", "[]", true),
+                Arguments.of("Single Element", "[1]", true),
+                Arguments.of("Multiple Elements", "[1,2,3]", true),
+                Arguments.of("Missing Brackets", "1,2,3", false),
+                Arguments.of("Missing Commas", "[1 2 3]", false),
+                Arguments.of("Trailing Comma", "[1,2,3,]", false)
+        );
+    }
+
+    /**
+     * Asserts that the input matches the given pattern and returns the matcher
+     * for additional assertions.
+     */
+    private static Matcher test(String input, Pattern pattern, boolean success) {
+        Matcher matcher = pattern.matcher(input);
+        Assertions.assertEquals(success, matcher.matches());
+        return matcher;
+    }
+
+}
